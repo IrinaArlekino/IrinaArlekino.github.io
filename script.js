b862b05b3c88142ded5616bc0aec076ab47e2a1a
IrinaArlekino <160588706+IrinaArlekino@users.noreply.github.com>
2024-02-26 23:09:59 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-  $('.carusel').slick({
+$('.carusel-one').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  prevArrow: '<img class="strelka" src="вправо.png">',
+  prevArrow: '<img class="strelka" src="влево.png">',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+$('.carusel1-two').slick({
   dots: true,
   infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true,
-  slidesToScroll: 6,
+  speed: 500,
+  prevArrow: '<img class="strelka" src="вправо.png">',
+  prevArrow: '<img class="strelka" src="влево.png">',
+  fade: true,
+  cssEase: 'linear'
 });
